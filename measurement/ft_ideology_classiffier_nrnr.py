c0213fa1b0e6423fcb69c79dde5adad0d812a05a
davidm-mcgrath <78617014+davidm-mcgrath@users.noreply.github.com>
2025-08-20 15:31:16 -0400

    Create ft_ideology_classiffier_nrnr.py
    ideology measure for headlines + first paragraphs using custom fasttext embeddings trained on labeled text from the National Review and the New Republic
    
    run on HPC via bash

---
+++ measurement/ft_ideology_classiffier_nrnr.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+import numpy as np
+import re
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+import gensim.downloader as api
+import fasttext
+
+
+#load in model trained on new rep-nat rev
+model=fasttext.load_model('fasttext_ideology_model.ftz')
+#predict on local news data
+nptext_n=pd.read_csv("natnews_full_topics2.csv")
+test_texts=nptext_n['head_text'].to_list()
+#remove linebreaks
+clean_texts=[]
+for i in test_texts:
+    clean_texts.append(re.sub("\n","",i))
+
+#run model
+ft_label=[]
+ft_prob=[]
+for t in clean_texts:
+    label, prob = model.predict(t)
+    ft_label.append(label[0].replace('__label__', ''))
+    ft_prob.append(prob[0])
+
+index=nptext_n.iloc[:,1]
+nptext=pd.DataFrame(data={'X': index})
+
+nptext['ft_label']=ft_label
+nptext['ft_prob']=ft_prob
+
+nptext.to_csv('natnewsfull_ft_out.csv',index=False)
